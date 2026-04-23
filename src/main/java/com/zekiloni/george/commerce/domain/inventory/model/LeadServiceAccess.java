dedc6e8db281a9b1dc071c5f072d4e570beae8b8
Zekiloni <zekirija17@gmail.com>
2026-04-23 23:30:23 +0200

    Refactor `LeadServiceAccess`: handle null `leads` initialization, improve `addLeads` validation with null-safe checks.

--- src/main/java/com/zekiloni/george/commerce/domain/inventory/model/LeadServiceAccess.java
+++ src/main/java/com/zekiloni/george/commerce/domain/inventory/model/LeadServiceAccess.java
@@ -4,7 +4,9 @@ import com.zekiloni.george.platform.domain.model.Lead;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -15,10 +17,11 @@ public class LeadServiceAccess extends ServiceAccess {
     private List<Lead> leads;
 
     public void addLeads(List<String> leadIds) {
-            leadIds.forEach(leadId -> {
-                if (leads.stream().noneMatch(lead -> lead.getId().equals(leadId))) {
-                    leads.add(Lead.builder().id(leadId).build());
-                }
-            });
+        if (leads == null) leads = new ArrayList<>();
+        leadIds.forEach(leadId -> {
+            if (leads.stream().map(Lead::getId).filter(Objects::nonNull).noneMatch(leadId::equals)) {
+                leads.add(Lead.builder().id(leadId).build());
+            }
+        });
     }
 }
