c7d3abb3ccdbe1da63b4920205ce5ba8d93c28e9
Zekiloni <zekirija17@gmail.com>
2026-04-27 22:07:25 +0200

    Update `DomainEvent` documentation: enhance description to clarify purpose, usage, and significance of domain events in the system.

--- src/main/java/com/zekiloni/george/common/domain/model/DomainEvent.java
+++ src/main/java/com/zekiloni/george/common/domain/model/DomainEvent.java
@@ -1,8 +1,10 @@
 package com.zekiloni.george.common.domain.model;
 
 /**
- * Marker interfejs za sve domenske evente.
- * Omogućava uniforman rad sa domenskim eventima u sistemu.
+ * A marker interface for domain events. Domain events represent significant occurrences or changes
+ * in the state of the domain model that are relevant to the business logic.
+ * They are typically used to capture and communicate important events that happen within the system,
+ * allowing other parts of the application to react to these events in a decoupled manner.
  */
 public interface DomainEvent {
 }
